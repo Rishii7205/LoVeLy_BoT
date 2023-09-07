b7480f4b4db2e2913f285ad0d97eedacbd8ddb6f
Rishii_x_Janni <118364756+Rishii7205@users.noreply.github.com>
2023-09-07 23:22:18 +0530

    Update start.py

--- AnonX/utils/inline/start.py
+++ AnonX/utils/inline/start.py
@@ -1,33 +1,31 @@
 from typing import Union
 
-from pyrogram.types import InlineKeyboardButton
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 import config
-from AnonX import app
 
 
 def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text="🥺ᴀᴅᴅ ᴍᴇ ɪɴ ɢʀᴘ ʙᴀʙᴜ🥺",
+                text="🌹ᴀᴅᴅ ᴍᴇ ᴍᴏɪ ʟᴏᴠᴇ🌹",
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
         ],
         [
             InlineKeyboardButton(
-                text="ʜᴇʟᴩ",
-                callback_data="settings_back_helper",
-            ),
-            InlineKeyboardButton(
-                text="sᴇᴛᴛɪɴɢs", callback_data="settings_helper"
-            ),
+                text="❤️ᴏᴡɴᴇʀ❤️",
+                url=f"https://t.me/loveguruo",
+            )
         ],
         [
             InlineKeyboardButton(
-                text="ᴍᴀɪɴᴛᴀɪɴᴇʀ", user_id=OWNER),
+                text="💖ʜᴇʟᴘ💖",
+                callback_data="settings_back_helper",
+            ),
             InlineKeyboardButton(
-                text="sᴜᴩᴩᴏʀᴛ", url=f"{config.SUPPORT_GROUP}"
+                text="🌸sᴇᴛᴛɪɴɢs🌸", callback_data="settings_helper"
             ),
         ],
      ]
@@ -38,25 +36,25 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text="🥺ᴀᴅᴅ ᴍᴇ ɪɴ ɢʀᴘ ʙᴀʙᴜ🥺",
+                text="•🌷ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ🌷•",
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
-            ),
+            )
         ],
         [
             InlineKeyboardButton(
-                text="🥹ʜᴇʟᴩ🥹", callback_data="settings_back_helper"
+                text="🍒ᴄᴏᴍᴍᴀɴᴅs🍒", callback_data="settings_back_helper"
             ),
-        ],
-        [
-            InlineKeyboardButton(text="ᴍᴀɪɴᴛᴀɪɴᴇʀ", user_id=OWNER),
             InlineKeyboardButton(
-                text="🥰sᴜᴩᴩᴏʀᴛ🥰", url=f"{config.SUPPORT_GROUP}"
-            ),
+                text="🥀ғᴏᴜɴᴅᴇʀs🥀", user_id=OWNER
+            )
         ],
         [
             InlineKeyboardButton(
-                    text="💖sᴏᴜʀᴄᴇ💖", url=f"{config.UPSTREAM_REPO}"
-                )
+                text="💘ɢʀᴏᴜᴘ💘", url=config.SUPPORT_GROUP
+            ),
+            InlineKeyboardButton(
+                text="💞ᴜᴘᴅᴀᴛᴇs💞", url=f"https://t.me/loveguruoo"
+            )
         ],
      ]
     return buttons
